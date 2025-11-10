9d18d7640527cecc49aa9154a02f76e96d4c567a
raghav1482 <raghvendra1482002@gmail.com>
2025-11-10 23:22:10 +0530

    10/11   registration

---
+++ backendSpring/elect_billing/src/main/java/com/electricity/elect_billing/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.electricity.elect_billing.repository;
+
+import com.electricity.elect_billing.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+    User findByUsername(String username);
+}
---
+++ backendSpring/elect_billing/src/test/java/com/electricity/elect_billing/ElectBillingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.electricity.elect_billing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectBillingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
